0f4e088a448fe95e7149d2a7cee095ed7df5dee0
Aliya Zhumagulova <zhumagul100@gmail.com>
2021-03-10 22:31:36 +0600

    feat: update file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+ while (str.length>0) {
+var temp=str;
+     
+  for (var k=0;k<bracketsConfig.length;k++) {
+    var a=bracketsConfig[k];
+    
+   for (var t=0;t<str.length; t++)
+  {      var ind=str.indexOf(a[0],t);
+        var ind2=str.indexOf(a[1],t+1);
+      if(ind2==ind+1)  {
+     str=str.substring(0,ind)+str.substring(ind+1,ind2)+str.substring(ind2+1);  
+           
+    
+} 
+ } 
 }
+if (temp==str) {break;}
+ } 
+  
+if (str.length>0) {return false;}
+if (str.length==0) {return true;}
+} 
+
+
+
